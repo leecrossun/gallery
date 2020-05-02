af0430303b4d83f8c67645fed3296953a8117112
hyosunLee <leecrossun@gmail.com>
2020-05-03 06:51:19 +0900

    first commit likelion

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\hyosun\과제및활동\4학년1학기\멋쟁이사자\django\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
