712f8b5e9463b388f8fff1ea3847d950ab5756bd
hyosunLee <leecrossun@gmail.com>
2020-05-04 04:11:22 +0900

    blog proj

---
+++ blogproj/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('data published')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now=True)
+    score = models.IntegerField(null=True)
+    body = models.TextField(null=True)
+
+    def __str__(self):
+        return self.title
