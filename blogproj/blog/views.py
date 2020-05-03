f9760a98b5c6784d43e0148afb9f3a0448b4f9e9
hyosunLee <leecrossun@gmail.com>
2020-05-04 04:42:14 +0900

    remove directory

--- blogproj/blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Blog
-from .models import Movie
-# Create your views here.
-
-def home(request):
-    blogs = Blog.objects
-    movies = Movie.objects
-    return render(request, 'home.html', {'blogs':blogs, 'movies':movies})
